ea41da95084e2852a7439b2715da70c9c8f3ff22
Danila-Milovanov <9019853@student.zadkine.nl>
2026-03-26 18:29:05 +0100

    Layout.jsx

---
+++ src/components/Layout.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+
+function Layout({ children }) {
+    return (
+        <>
+        <Navbar />
+        <main className="p-6">
+            {children}
+        </main>
+        </>
+    );
+}
+
+export default Layout;
