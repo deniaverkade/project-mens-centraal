41cfa44247e31998826c2af3b2231474e510b9a5
deniaverkade <9024406@student.zadkine.nl>
2026-04-09 12:58:54 +0200

    npm install opnieuw gedaan, homepage basis gemaakt
    ik kreeg website niet te praat zonder weer npm install te doen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,26 @@
 const Home = () => {
     return ( <section>
-        <h1>Homepage W.I.P</h1>
+        <div class="bg-pink-600 w-145 h-45 absolute mt-75 border-pink-900 border-t-9 border-r-9 border-b-9 opacity-60">
+        <p class="text-black opacity-100 text-center mt-15 text-2xl">Bij stichting mens centraal etc</p>
+        </div>
+        <img src="../public/images/placeholder.jpeg" class="w-full h-130" />
+        <div class="flex gap-60 ml-18">
+            <div>
+                <h1 class="text-3xl">Lorem ipsum</h1>
+                <p>Lorem ipsum</p>
+            </div>
+
+            <div>
+                <h1 class="text-3xl">Lorem ipsum</h1>
+                <p>Lorem ipsum</p>
+            </div>
+
+            <div>
+                <h1 class="text-3xl">Lorem ipsum</h1>
+                <p>Lorem ipsum</p>
+            </div>
+        </div>
+        
     </section> );
 }
  
